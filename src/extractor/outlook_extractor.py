da195a78ae577f9a813547811b8f104b87410ecd
BumbleFlash <sudarshan97@gmail.com>
2021-02-25 22:10:25 -0500

    chore: removed links from message body

--- src/extractor/outlook_extractor.py
+++ src/extractor/outlook_extractor.py
@@ -1,4 +1,5 @@
 import win32com.client
+import re
 from src.constants import email_constants
 
 
@@ -8,10 +9,13 @@ class OutlookExtractor(object):
         pass
 
     def extract_data(self):
-        self.to_suppress_static_warning()
         outlook = win32com.client.Dispatch(email_constants.OUTLOOK_APPLICATION).GetNamespace(
             email_constants.MAPI_NAMESPACE)
         inbox = outlook.GetDefaultFolder(email_constants.INBOX_FOLDER)
         messages = inbox.Items
-        for message in messages:
-            print(message.SenderEmailAddress)
+        message = messages.GetLast()
+        print(self.remove_links(message.body))
+
+    def remove_links(self, body):
+        self.to_suppress_static_warning()
+        return re.sub(r'http\S+', '', body)
